3c8fddacb873808adad7cdb9197643142a69deae
GURNEETGN <gurneetsingh86483@gmail.com>
2025-02-02 15:40:42 +0530

    some changes are done in files

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import{ChartComponent,Series,SeriesCollectionDirective,Inject,DateTime,Legend,ToolTip,LineSeries} from '@syncfusion/ej2-react-charts'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesDirective,
+  SeriesCollectionDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+  LineSeries,
+} from "@syncfusion/ej2-react-charts";
 
-import {lineCustomSeries,LinePrimaryYAxix,LinePrimaryXAxix} from '../../data/dummy'
+import {
+  lineCustomSeries,
+  LinePrimaryYAxis,
+  LinePrimaryXAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 const LineChart = () => {
+  const { currentMode } = useStateContext();
   return (
     <div>
-      <ChartComponent>
-        <Inject services={[LineSeries,DateTime,Legend,ToolTip]}/>
+      <ChartComponent
+        id="line-chart"
+        height="420px"
+        primaryXAxis={LinePrimaryXAxis}
+        primaryYAxis={LinePrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === 'Dark' ? '#33373E':'#fff'}
+      >
+        <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+        <SeriesCollectionDirective>
+          {lineCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
       </ChartComponent>
     </div>
-  )
-}
+  );
+};
 
-export default LineChart
+export default LineChart;
